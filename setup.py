23768e84759881fd14f9f841bd9ddbc18f45dfea
Srikanth Gedela <ubuntu22@SriDellXPS15>
2022-06-12 13:37:30 +0100

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='cookie Cutter template for Machine learning Project',
+    author='Srikanth',
+    license='MIT',
+)
